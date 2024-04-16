51342a55fe5756904ea3f474865b8d987111a5b3
Misha <mihanix.eth@gmail.com>
2024-04-16 15:44:08 +0300

    prettify merkle tree builder

--- services/tx_root_publisher/merkle_tree.py
+++ services/tx_root_publisher/merkle_tree.py
@@ -1,51 +1,85 @@
+import abc
 import hashlib
 import typing as t
 
-class Node:
+
+class _NodeMeta(metaclass=abc.ABCMeta):
+    @staticmethod
+    @abc.abstractmethod
+    def hash_func(_: str) -> bytes:
+        ...
+    
+    @staticmethod
+    @abc.abstractmethod
+    def is_valid(_: str) -> bool:
+        ...
+
+    @staticmethod
+    @abc.abstractmethod
+    def delim() -> str:
+        ...
+
+
+class TNode(_NodeMeta):
+    hash_func: t.Callable[[str], bytes] = lambda _: hashlib.sha256(_.encode()).digest()
+    is_valid: t.Callable[[str], bool] = lambda _: True
+    delim: str = ":"
+
+    __slots__ = (
+        'hash_value',
+        'orig_value',
+        'l_node',
+        'r_node'
+    )
+
     hash_value: t.Optional[bytes]
-    source_value: t.Optional[str]
-    left_node: t.Optional['Node']
-    right_node: t.Optional['Node']
+    orig_value: t.Optional[dict]
+    l_node: t.Optional['TNode']
+    r_node: t.Optional['TNode']
 
     def __init__(self):
-        self.source_value = None
+        self.orig_value = None
         self.hash_value = None
-        self.left_node = None
-        self.right_node = None
+        self.l_node = None
+        self.r_node = None
 
-    def set_value(self, value: str):
-        self.source_value = value
-        self.hash_value = hashlib.sha256(value.encode()).digest()
-
-    def get_value(self):
-        return "" if self.source_value is None else self.source_value
+    @property
+    def value(self):
+        return "" if self.orig_value is None else self.orig_value
+    
+    @value.setter
+    def value(self, x: str):
+        self.orig_value = x
+        self.hash_value = TNode.hash_func(self.orig_value)
 
     def __repr__(self):
-        return f"[hash: {self.hash_value.hex()}, orig: {self.source_value}]"
+        return f"[hash: {self.hash_value.hex()}, orig: {self.orig_value}]"
 
-def build_tree(values: list[str]):
-    node = Node()
+
+def build_tree(values: list[str]) -> t.Optional['TNode']:
     if len(values) > 1:
-        mid_point = len(values) // 2
-        node = Node()
-        node.left_node = build_tree(values[0:mid_point])
-        node.right_node = build_tree(values[mid_point:len(values)])
-        print("left", node.left_node)
-        print("rifght", node.right_node)
-        node.set_value(node.left_node.get_value() + ":" + node.right_node.get_value())
+        split: int = len(values) // 2
+        node = TNode()
+        node.l_node = build_tree(values[0:split])
+        node.r_node = build_tree(values[split:len(values)])
+        print("left:", node.l_node)
+        print("rifght:", node.r_node)
+        node.value = node.l_node.value + TNode.delim + node.r_node.value
         return node
     elif len(values) == 1:
-        node = Node()
-        node.set_value(values[0])
+        node = TNode()
+        node.value = values[0]
         return node
     else:
         return None
+# todo: compare hashes 
 
-def main():
-    test_values = ["a", "b", "c", "d"]
-    root_node = build_tree(test_values)
+
+def test():
+    test_values: list[str] = ["a", "b", "c", "d", "e", "f", "g"]
+    root_node: TNode = build_tree(test_values)
     print(root_node)
 
 
 if __name__ == '__main__':
-    main()
+    test()
