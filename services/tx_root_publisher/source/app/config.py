83283be5b196283ba9a6466bf4177e799300a34a
Misha <mihanix.eth@gmail.com>
2024-04-17 19:31:39 +0300

    add interactions abi

--- services/tx_root_publisher/source/app/config.py
+++ services/tx_root_publisher/source/app/config.py
@@ -1,7 +1,26 @@
 import os
 
-class PgConfig:
-    # POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_DB = os.getenv("POSTGRES_DB")
-    POSTGRES_USER = os.getenv("POSTGRES_USER")
-    POSTGRES_HOST = "postgres"
+
+class ContractsRelayerConfig:
+    INTERACTION_DATASET_ADDR = os.getenv("INTERACION_DATASET_ADDR")
+    RELAYER_PK: str = os.getenv("RELAYER_PK")
+    DATASET_ABI: dict = {
+        "abi": [{
+            "type": "function",
+            "name": "updateRoot",
+            "inputs": [
+                {
+                    "name": "nextMerkleRoot",
+                    "type": "bytes32",
+                    "internalType": "bytes32"
+                }, 
+                {
+                    "name": "nextProofsHash",
+                    "type": "bytes32",
+                    "internalType": "bytes32"
+                }
+            ],
+            "outputs": [],
+            "stateMutability": "nonpayable"
+        }]
+    }
